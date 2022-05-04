8e1892444d458ee78e0e2e4684f9374bc57ad7ad
Tim <Tim280696@hotmail.com>
2022-05-04 16:05:27 +0200

    added first section

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,26 @@
 import '../styles/Home.scss'
 import Header from '../Components/Header'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
     <main className="home">
       <Header />
+      <section className="infos-section">
+        <div className="infos-text-content">
+          <h1 className="infos-title">More than just shorter links</h1>
+          <p className="infos-paragraph">
+            Build your brand's recognition and get detailed insights on how your
+            links are performing.
+          </p>
+          <Link to="/error404">
+            <button className="started-btn">Get Started</button>
+          </Link>
+        </div>
+        <div className="infos-image-content">
+          <div className="infos-image"></div>
+        </div>
+      </section>
     </main>
   )
 }
