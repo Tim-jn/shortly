5c4fceb199dd35df2afdf23a72883ca38e71b191
Tim <Tim280696@hotmail.com>
2022-05-12 14:56:17 +0200

    fixed shorten link

--- src/Components/ShortenLink.tsx
+++ src/Components/ShortenLink.tsx
@@ -1,41 +1,67 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import '../styles/ShortenLink.scss'
 
 export default function ShortenLink() {
   const [valid, setValid] = useState<Boolean>(true)
   const [items, setItems] = useState<any[]>([])
-  const [linkToShorten, setLinkToShorten] = useState('')
-  const [shortenedLink, setShortenedLink] = useState('')
 
   // function for submitting a link
 
-  const handleSubmit = async (e: React.SyntheticEvent) => {
+  const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault()
+
     const target = e.target as typeof e.target & {
       input: { value: string }
     }
 
+    const input = target.input.value
+
     const regexUrl =
       /https?:\/\/(www\.)?[-a-zA-Z0-9@:%.+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi
 
-    const input = target.input.value
-    setLinkToShorten(input)
     const id = Math.floor(Math.random() * (100 - 6 + 1)) + 6
 
-    if (input && input.match(regexUrl)) {
+    if (input.match(regexUrl)) {
       setValid(true)
-      let copy: any[] = [...items]
-      copy = [...copy, { id, input, active: false, shortUrl: shortenedLink }]
-      setItems(copy)
+      getFetched(input, id)
     } else {
       setValid(false)
     }
   }
 
+  // get data and return an object
+
+  const getFetched = async (input: string, id: number) => {
+    await fetch(`https://api.shrtco.de/v2/shorten?url=${input}`, {
+      method: 'POST',
+    })
+      .then((res) => res.json())
+      .then(
+        (response) => {
+          let copy: any[] = [...items]
+          copy = [
+            ...copy,
+            {
+              id,
+              input,
+              active: false,
+              shortUrl: response.result?.full_short_link,
+            },
+          ]
+          setItems(copy)
+        },
+        (error) => {
+          console.log(error)
+
+          return error
+        }
+      )
+  }
+
   // function timer to copy a shortened link
 
   const handleClick = (item: any) => {
-    navigator.clipboard.writeText(item.input)
+    navigator.clipboard.writeText(item.shortUrl)
 
     let mapped = items.map((i) => {
       return i.id === parseInt(item.id) ? { ...i, active: true } : { ...i }
@@ -51,23 +77,6 @@ export default function ShortenLink() {
 
     setItems(mapped)
   }
-
-  // get data
-
-  useEffect(() => {
-    fetch(`https://api.shrtco.de/v2/shorten?url=${linkToShorten}`, {
-      method: 'POST',
-    })
-      .then((res) => res.json())
-      .then(
-        (response) => {
-          setShortenedLink(response.result?.full_short_link)
-        },
-        (error) => {
-          console.log(error)
-        }
-      )
-  }, [linkToShorten])
 
   return (
     <div className="shorten-link-content">
@@ -87,36 +96,37 @@ export default function ShortenLink() {
         />
         {valid ? '' : <p className="warning">Please add a link</p>}
       </form>
-      {items.map((item: any, key: number) => {
-        return (
-          <div key={key} className="new-shortened-link">
-            <p className="link">{item.input}</p>
-            <div className="shortened-link">
-              <a
-                href={item.shortUrl}
-                target="_blank"
-                rel="noreferrer"
-                className="shorten-link"
-              >
-                {item.shortUrl}
-              </a>
-              <button
-                className={
-                  !item.active
-                    ? 'copy-button'
-                    : 'copy-button copy-button-active'
-                }
-                onClick={() => {
-                  handleClick(item)
-                  setTimeout(() => removeHandleClick(), 1000)
-                }}
-              >
-                {!item.active ? 'Copy' : 'Copied!'}
-              </button>
+      {items.length >= 1 &&
+        items.map((item: any, key: number) => {
+          return (
+            <div key={key} className="new-shortened-link">
+              <p className="link">{item.input}</p>
+              <div className="shortened-link">
+                <a
+                  href={item.shortUrl}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="shorten-link"
+                >
+                  {item.shortUrl}
+                </a>
+                <button
+                  className={
+                    !item.active
+                      ? 'copy-button'
+                      : 'copy-button copy-button-active'
+                  }
+                  onClick={() => {
+                    handleClick(item)
+                    setTimeout(() => removeHandleClick(), 1000)
+                  }}
+                >
+                  {!item.active ? 'Copy' : 'Copied!'}
+                </button>
+              </div>
             </div>
-          </div>
-        )
-      })}
+          )
+        })}
     </div>
   )
 }
