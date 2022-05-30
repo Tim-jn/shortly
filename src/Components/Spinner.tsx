8f589b9646cda97b3d73047167b970e4a559965f
Tim <Tim280696@hotmail.com>
2022-05-30 10:41:58 +0200

    Updated spinner

--- src/Components/Spinner.tsx
+++ src/Components/Spinner.tsx
@@ -4,6 +4,7 @@ export default function Spinner() {
   return (
     <div className="spinner-container">
       <div className="loading-spinner"></div>
+      <p className="spinner-text">API may take a while to respond sometimes</p>
     </div>
   )
 }
